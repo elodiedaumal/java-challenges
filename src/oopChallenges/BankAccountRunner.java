1c220391cb9ed758c05b2afe61b460e87ac3302d
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-12-08 09:05:22 +0100

    using constructors on bank account method

--- src/oopChallenges/BankAccountRunner.java
+++ src/oopChallenges/BankAccountRunner.java
@@ -2,12 +2,26 @@ package oopChallenges;
 
 public class BankAccountRunner {
     public static void main(String[] args) {
-        BankAccount bankAccount = new BankAccount();
-        bankAccount.deposit(500);
-        System.out.println(bankAccount.getAccountBalance());
-        bankAccount.deposit(-500);
-        bankAccount.withdraw(-500);
-        System.out.println(bankAccount.getAccountBalance());
 
+        Account elodieAccount = new Account("12345", 1500,
+                "Elodie Daumal", "myemail@elodie.com",
+                "614 168 008");
+
+        System.out.println(elodieAccount.getNumber());
+        System.out.println(elodieAccount.getBalance());
+
+
+
+        elodieAccount.withdrawFunds(100.0);
+        elodieAccount.depositFunds(250);
+        elodieAccount.withdrawFunds(50);
+
+        elodieAccount.withdrawFunds(200);
+
+        elodieAccount.depositFunds(100);
+        elodieAccount.withdrawFunds(45.55);
+        elodieAccount.withdrawFunds(54.46);
+
+        elodieAccount.withdrawFunds(54.45);
     }
 }
