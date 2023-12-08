10d8bc844b79b23a252913fecbb9c899e3eb80af
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-12-08 17:01:21 +0100

    change the employee id to be dinamyc

--- src/inheritence/challenge/Employee.java
+++ src/inheritence/challenge/Employee.java
@@ -5,9 +5,11 @@ public class Employee extends Worker {
         private long employeeId;
         private String hireDate;
 
+        private static int employeeNo=1;
+
         public Employee(String name, String dateOfBirth, String endDate, long employeeId, String hireDate) {
             super(name, dateOfBirth, endDate);
-            this.employeeId = employeeId;
+            this.employeeId = Employee.employeeNo++;
             this.hireDate = hireDate;
         }
 
