306b5f3ffafa51f47d955d58a8490bcab478d201
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2024-01-23 10:37:09 +0100

    Array exercises

---
+++ src/arrays/CompareArray.java
@@ -0,0 +1,13 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class CompareArray {
+    public static void main(String[] args) {
+        int[] s1={1,2,3,4,5,6};
+        int[] s2={1,2,3,4,5,6};
+        if (Arrays.equals(s1,s2) ) {
+            System.out.println("the Arrays are the same");
+        }
+    }
+}
