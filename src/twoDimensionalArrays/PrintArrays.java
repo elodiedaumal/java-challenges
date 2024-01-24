5ff2780836241d17766c56ccb04f289dda7c8871
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2024-01-24 10:59:09 +0100

    exemple of two dimentional arrays

--- src/twoDimensionalArrays/PrintArrays.java
+++ src/twoDimensionalArrays/PrintArrays.java
@@ -22,5 +22,7 @@ public class PrintArrays {
         }
 
         System.out.println(Arrays.deepToString(array2));
+        array2[1] = new int[] {1,20,30};
+        System.out.println(Arrays.deepToString(array2));
     }
 }
